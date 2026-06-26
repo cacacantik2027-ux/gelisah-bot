cc5b60fbef1e4afa5063bf2e3b5e59ff9e1813a3
cacacantik2027-ux <cacacantik2027@gmail.com>
2026-06-26 21:03:19 +0700

    Add files via upload

---
+++ bot1.py
@@ -0,0 +1,7 @@
+# config.py
+BOT_TOKEN = "TOKEN_BOT_ANDA"
+LIVECHAT_BOT = "USERNAME_BOT_LIVECHAT_ANDA"
+ADMIN_IDS = [123456789, 987654321]
+LOG_GROUP_ID = -1001234567890 # ID Grup Log Chat Admin Anda (biasanya diawali -100)
+QRIS_IMAGE_URL = "https://link-gambar-qris-anda.com/qris.jpg" # Bisa link gambar atau file_id telegram
+BANK_INFO = "BCA: 1234567890 a.n GELISAH\nDANA: 081234567890"
